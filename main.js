f2a44b811ebfd5ba8476383366cc15a9c200af4e
omer gehad <68555369+omergehad405@users.noreply.github.com>
2023-03-25 04:18:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let input = document.querySelector("#task");
+let addBtn = document.querySelector("#add");
+let listContainer = document.querySelector("#list-container");
+
+addBtn.addEventListener("click" , function() {
+    if(input.value === ''){
+        alert("You Must Enter A Task")
+    }else{
+        let li = document.createElement("li");  
+        li.innerHTML = input.value;
+        listContainer.append(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input.value = "";
+    saveData();
+});
+
+listContainer.addEventListener("click" , function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+} , false );
+
+function saveData() {
+    localStorage.setItem("data" , listContainer.innerHTML)
+}
+
+function showData() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showData() ;
